a12eea13e248f2832af0b7424f88aa3ff80c781c
Kamil Krampa <kkrampa@soldevelo.com>
2014-07-10 15:30:30 +0200

    Code refactoring

--- custom/ilsgateway/commtrack.py
+++ custom/ilsgateway/commtrack.py
@@ -1,12 +1,15 @@
 import logging
-from dimagi.utils.django.email import send_HTML_email
 from django.contrib.auth.models import User
 from corehq import Domain
-from corehq.apps.users.models import WebUser, CommCareUser, CouchUser, DomainMembership
+from corehq.apps.users.models import WebUser, CommCareUser, CouchUser, UserRole
 from custom.api.utils import apply_updates
 from custom.ilsgateway.api import ILSGatewayEndpoint
 from corehq.apps.commtrack.models import Product
 from dimagi.utils.dates import force_to_datetime
+from custom.ilsgateway.models import MigrationCheckpoint
+from requests.exceptions import ConnectionError
+from datetime import datetime
+
 
 
 def sync_ilsgateway_product(domain, ilsgateway_product):
@@ -28,8 +31,6 @@ def sync_ilsgateway_product(domain, ilsgateway_product):
 
 
 def sync_ilsgateway_webusers(domain, ilsgateway_webuser):
-
-
     user = WebUser.get_by_username(ilsgateway_webuser.email.lower())
     user_dict = {
         'first_name': ilsgateway_webuser.first_name,
@@ -62,6 +63,7 @@ def sync_ilsgateway_webusers(domain, ilsgateway_webuser):
 
     return user
 
+
 def sync_ilsgateway_smsusers(domain, ilsgateway_smsuser):
     username_part = "%s%d" % (ilsgateway_smsuser.name.strip().replace(' ', '.').lower(), ilsgateway_smsuser.id)
     username = "%s@%s.commcarehq.org" % (username_part, domain.name)
@@ -69,8 +71,22 @@ def sync_ilsgateway_smsusers(domain, ilsgateway_smsuser):
     splitted_value = ilsgateway_smsuser.name.split(' ', 1)
     first_name = last_name = ''
     if splitted_value:
-        first_name = splitted_value[0][:30] #due to postgres restriction
+        first_name = splitted_value[0][:30]
         last_name = splitted_value[1][:30] if len(splitted_value) > 1 else ''
+
+    user_dict = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'is_active': bool(ilsgateway_smsuser.is_active),
+        'email': ilsgateway_smsuser.email
+        #TODO location
+    }
+
+    if ilsgateway_smsuser.phone_numbers:
+        user_dict['phone_numbers'] = [ilsgateway_smsuser.phone_numbers[0].replace('+', '')]
+        user.user_data = {
+            'backend': ilsgateway_smsuser.backend
+        }
 
     if user is None and username_part:
         try:
@@ -80,21 +96,57 @@ def sync_ilsgateway_smsusers(domain, ilsgateway_smsuser):
             user.first_name = first_name
             user.last_name = last_name
             user.is_active = bool(ilsgateway_smsuser.is_active)
-            if ilsgateway_smsuser.phone_numbers:
-                user.set_default_phone_number(ilsgateway_smsuser.phone_numbers[0].replace('+', ''))
-                user.user_data = {
-                    'backend': ilsgateway_smsuser.backend
-                }
-                user.save()
+            user.save()
         except Exception as e:
             logging.error(e)
-
+    else:
+        if apply_updates(user, user_dict):
+            user.save()
     return user
 
 
+def products_sync(domain, endpoint):
+    for product in endpoint.get_products():
+            sync_ilsgateway_product(domain, product)
+
+
+def webusers_sync(project, endpoint):
+    for user in endpoint.get_webusers():
+        if user.email:
+            if not user.is_superuser:
+                setattr(user, 'role_id', UserRole.get_read_only_role_by_domain(project.name).get_id)
+            sync_ilsgateway_webusers(project, user)
+
+def smsusers_sync(project, endpoint):
+    has_next = True
+    next_url = None
+    while has_next:
+        next_url_params = next_url.split('?')[1] if next_url else None
+        meta, users = endpoint.get_smsusers(next_url_params)
+        for user in users:
+            sync_ilsgateway_smsusers(project, user)
+
+        if not meta['next']:
+            has_next = False
+        else:
+            next_url = meta['next']
+
 def bootstrap_domain(domain):
     project = Domain.get_by_name(domain)
-    if project.commtrack_settings and project.commtrack_settings.ilsgateway_config.is_configured:
-        endpoint = ILSGatewayEndpoint.from_config(project.commtrack_settings.ilsgateway_config)
-        for product in endpoint.get_products():
-            sync_ilsgateway_product(domain, product)
+    start_date = datetime.today()
+    endpoint = ILSGatewayEndpoint.from_config(project.commtrack_settings.ilsgateway_config, project.name)
+    try:
+        products_sync(project, endpoint)
+        webusers_sync(project, endpoint)
+        smsusers_sync(project, endpoint)
+        try:
+            checkpoint = MigrationCheckpoint.objects.get(domain=project.name)
+            checkpoint.date = start_date
+            checkpoint.save()
+        except MigrationCheckpoint.DoesNotExist:
+            checkpoint = MigrationCheckpoint()
+            checkpoint.domain = project.name
+            checkpoint.date = start_date
+            checkpoint.save()
+    except ConnectionError as e:
+        logging.error(e)
