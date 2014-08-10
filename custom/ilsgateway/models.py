142a7a4f75cf1989f2bef5e1a8b393799ca57d19
Kamil Krampa <kkrampa@soldevelo.com>
2014-08-10 17:37:37 +0200

    Changed ilsgateway config

--- custom/ilsgateway/models.py
+++ custom/ilsgateway/models.py
@@ -1,5 +1,44 @@
+from couchdbkit.ext.django.schema import Document, BooleanProperty, StringProperty
 from django.db import models
+from casexml.apps.stock.models import DocDomainMapping
+
 
 class MigrationCheckpoint(models.Model):
      domain = models.CharField(max_length=100)
      date = models.DateTimeField()
+
+
+class ILSGatewayConfig(Document):
+    enabled = BooleanProperty(default=False)
+    domain = StringProperty()
+    url = StringProperty()
+    username = StringProperty()
+    password = StringProperty()
+
+    @classmethod
+    def for_domain(cls, name):
+        try:
+            mapping = DocDomainMapping.objects.get(domain_name=name, doc_type='ILSGatewayConfig')
+            return cls.get(docid=mapping.doc_id)
+        except DocDomainMapping.DoesNotExist:
+            return None
+
+    @classmethod
+    def get_all_domains(cls):
+        mappings = DocDomainMapping.objects.filter(doc_type='ILSGatewayConfig')
+        domains = [cls.get(docid=mapping.doc_id) for mapping in mappings]
+        return domains
+
+    @property
+    def is_configured(self):
+        return True if self.enabled and self.url and self.password and self.username else False
+
+    def save(self, **params):
+        super(ILSGatewayConfig, self).save(**params)
+        try:
+            DocDomainMapping.objects.get(doc_id=self._id,
+                                         domain_name=self.domain,
+                                         doc_type="ILSGatewayConfig")
+        except DocDomainMapping.DoesNotExist:
+            DocDomainMapping.objects.create(doc_id=self._id, doc_type='ILSGatewayConfig',
+                                        domain_name=self.domain)
--- custom/ilsgateway/tasks.py
+++ custom/ilsgateway/tasks.py
@@ -1,12 +1,20 @@
 from celery.task import task, periodic_task
-from corehq.apps.commtrack.models import ILSGatewayConfig
 from corehq.apps.domain.models import Domain
 from custom.ilsgateway.commtrack import bootstrap_domain
 
 
 #@periodic_task(run_every=timedelta(days=1), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
+from custom.ilsgateway.models import ILSGatewayConfig
+
+
 def migration_task():
-    projects = Domain.get_all()
+    configs = ILSGatewayConfig.get_all_domains()
+    projects = []
+    print configs
+    for config in configs:
+        if config.enabled:
+            projects.append(config.domain)
+
     for project in projects:
         ilsgateway_config = ILSGatewayConfig.for_domain(project)
         if ilsgateway_config:
