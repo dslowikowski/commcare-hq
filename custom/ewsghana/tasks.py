a59669386d5ac05054af4f5355ca2472b7246584
Kamil Krampa <kkrampa@soldevelo.com>
2014-12-03 17:02:09 +0100

    Code cleanup

--- custom/ewsghana/tasks.py
+++ custom/ewsghana/tasks.py
@@ -25,6 +25,8 @@ EXTENSIONS = {
 
 
 LOCATION_TYPES = ["country", "region", "district", "facility"]
+# District Ashanti
+EWS_FACILITIES = [109, 110, 624, 626, 922, 908, 961, 948, 956, 967]
 
 
 # @periodic_task(run_every=timedelta(days=1), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
@@ -41,74 +43,6 @@ def ews_bootstrap_domain_task(domain):
     ews_config = EWSGhanaConfig.for_domain(domain)
     commtrack_settings_sync(domain, LOCATION_TYPES)
     return bootstrap_domain(ews_config, GhanaEndpoint.from_config(ews_config), EXTENSIONS, fetch_groups=False)
-
-# District Ashanti
-EWS_FACILITIES = [109, 110, 624, 626, 922, 908, 961, 948, 956, 967]
-
-
-@task
-def ews_stock_data_task(domain):
-    ewsghana_config = EWSGhanaConfig.for_domain(domain)
-    domain = ewsghana_config.domain
-
-    start_date = datetime.today()
-    try:
-        checkpoint = StockDataCheckpoint.objects.get(domain=domain)
-        api = checkpoint.api
-        date = checkpoint.date
-        limit = checkpoint.limit
-        offset = checkpoint.offset
-        location = checkpoint.location
-        if not checkpoint.start_date:
-            checkpoint.start_date = start_date
-            checkpoint.save()
-        else:
-            start_date = checkpoint.start_date
-    except StockDataCheckpoint.DoesNotExist:
-        checkpoint = StockDataCheckpoint()
-        checkpoint.domain = domain
-        checkpoint.start_date = start_date
-        api = 'product_stock'
-        date = None
-        limit = 100
-        offset = 0
-        location = None
-
-    endpoint = GhanaEndpoint.from_config(ewsghana_config)
-    if TEST:
-        commtrack_settings_sync(domain, LOCATION_TYPES)
-        for product in endpoint.get_products():
-            sync_ilsgateway_product(domain, product)
-        get_locations(domain, endpoint, EWS_FACILITIES)
-        facilities = EWS_FACILITIES
-    else:
-        facilities = SQLLocation.objects.filter(
-            domain=domain,
-            location_type__iexact='FACILITY'
-        ).order_by('created_at').values_list('external_id', flat=True)
-    apis = (
-        ('product_stock', partial(get_product_stock, domain=domain, endpoint=endpoint,
-                                  checkpoint=checkpoint, date=date, start_date=start_date)),
-        ('stock_transaction', partial(get_stock_transaction, domain=domain, endpoint=endpoint,
-                                      checkpoint=checkpoint, date=date, start_date=start_date))
-    )
-    apis_from_checkpoint = itertools.dropwhile(lambda x: x[0] != api, apis)
-    facilities_copy = list(facilities)
-    if location:
-        facilities = itertools.dropwhile(lambda x: int(x) != int(location.external_id), facilities)
-    else:
-        facilities = facilities
-    for idx, api in enumerate(apis_from_checkpoint):
-        print api[0], limit, offset
-        api[1](limit=limit, offset=offset, facilities=facilities)
-        limit = 100
-        offset = 0
-        if idx == 0:
-            facilities = facilities_copy
-
-    save_stock_data_checkpoint(checkpoint, 'product_stock', 100, 0, start_date, None, False)
-    checkpoint.start_date = None
-    checkpoint.save()
 
 
 @task
